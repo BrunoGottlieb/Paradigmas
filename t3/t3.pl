4017b3cd31bf059b8e37540b49e8d3765028bf6a
BrunoGottlieb <48501861+BrunoGottlieb@users.noreply.github.com>
2019-05-01 13:31:28 -0300

    Adicionada questão 5

--- t3/t3.pl
+++ t3/t3.pl
@@ -1,3 +1,5 @@
+%BrunoGottlieb
+
 %1. Defina um predicado odd(N) que seja verdadeiro se N for um número ímpar
 odd(X) :- 1 is mod(X, 2).
 
@@ -11,8 +13,14 @@ inc99([H|T],[H2|T2]) :-
     H2 is H+99,
     inc99(T,T2).
 
-%4. Defina um predicado recursivo incN(L1,L2,N), de forma que L2 seja uma lista com todos os elementos de L1 acrescidos da constante N.
+%4. Defina um predicado recursivo incN(L1,L2,N), de forma que L2 seja uma lista com todos os elementos de L1 acrescidos da constante N
 incN([],[],_).
 incN([H|T],[H2|T2],N) :-
 	H2 is H+N,
 	incN(T,T2,N).
+
+%5. Defina um predicado recursivo comment(L1,L2), de forma que L1 seja uma lista de strings e L2 tenha todos os elementos de L1 concatenados com o prefixo "%%"
+comment([],[]).
+comment([H|T],[H2|T2]) :-
+	string_concat(H,"%%",H2),
+	comment(T,T2).
