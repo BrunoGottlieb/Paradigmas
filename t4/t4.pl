6ee2022437a2f9ec045fc92ceadf3e9a6938d42a
BrunoGottlieb <48501861+BrunoGottlieb@users.noreply.github.com>
2019-05-01 21:34:26 -0300

    Atualização t4

--- t4/t4.pl
+++ t4/t4.pl
@@ -1,3 +1,5 @@
+%BrunoGottlieb
+
 dinheiro(alice, rico).
 dinheiro(henrique, rico).
 dinheiro(adriano, rico).
@@ -10,32 +12,41 @@ dinheiro(bernardo, pobre).
 insano(adriano).
 insano(maria).
 
-arma(maria).
-arma(pedro).
-arma(henrique).
-arma(adriano).
-arma(alice).
+santaMaria(pedro, Y) :- Y = segunda,! ; Y = terca,! ; Y = quinta,!.
+santaMaria(bernardo, Y) :- Y = segunda,! ; Y = terca,! ; Y = quinta,!.
+santaMaria(maria, Y) :- Y = terca,! ; Y = quarta,! ; Y = quinta,!.
+santaMaria(adriano, quarta).
+santaMaria(caren, quinta).
+santaMaria(bia, quinta).
 
-apartamento(pedro).
-apartamento(caren).
-apartamento(henrique).
-apartamento(bia).
-apartamento(adriano).
-apartamento(alice).
-apartamento(bernardo).
-apartamento(maria).
+portoAlegre(caren, Y) :- Y = segunda,! ; Y = terca,! ; Y = quarta,!.
+portoAlegre(henrique, terca).
+portoAlegre(bia, Y) :- Y = terca,! ; Y = quarta,!.
+portoAlegre(alice, Y) :- Y = terca,! ; Y = quarta,!.
+portoAlegre(bernardo, quarta).
+portoAlegre(pedro, quarta).
 
-chave(maria).
-chave(caren).
-chave(henrique).
-chave(bia).
-chave(alice).
+apartamento(bia, Y) :- Y = segunda,! ; Y = sexta,!.
+apartamento(maria, Y) :- Y = segunda,! ; Y = sexta,!.
+apartamento(alice, Y) :- Y = segunda,! ; Y = quinta,! ; Y = sexta,!.
+apartamento(henrique, Y) :- Y \= terca.
+apartamento(adriano, Y) :- Y \= quarta.
+apartamento(caren, sexta).
+apartamento(bernardo, sexta).
 
 ciumes(alice).
 ciumes(caren).
 
+bastao(X) :- portoAlegre(X, quinta) ; santaMaria(X, quarta).
+
+martelo(X) :- apartamento(X, quarta) ; apartamento(X, quinta).
+
+chave(X) :- santaMaria(X, quarta) ; portoAlegre(X, terca).
+
+arma(X) :- bastao(X) ; martelo(X).
+
 motivo(X) :- dinheiro(X, pobre) ; insano(X) ; ciumes(X).
 
-acesso(X) :- arma(X) , apartamento(X) , chave(X).
+acesso(X) :- (apartamento(X, sexta) ; apartamento(X, quinta)) , chave(X).
 
-assassino(X) :- motivo(X) , acesso(X).
+assassino(X) :- motivo(X) , acesso(X) , arma(X).
